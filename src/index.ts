a2b2ebdd6ee1a6afcd874e81e762e0a49daa2806
adilraja11 <adilraja19@students.undip.ac.id>
2024-12-02 11:49:48 +0700

    add auth API

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { Elysia, t } from "elysia";
 import {swagger} from "@elysiajs/swagger"
 import { noteRouter } from "./routes/noteRouter";
+import { authRouter } from "./routes/authRouter";
 
 const app = new Elysia()
   .use(swagger({
     path: "/docs"
   }))
   .use(noteRouter)
+  .use(authRouter)
   .listen(3000);
 
 console.log("NoteBE ran at 3000")
