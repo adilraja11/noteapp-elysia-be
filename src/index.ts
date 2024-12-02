ec34cdbab2dd7d29a970cf411a2fa7f0ae299e8d
adilraja11 <adilraja19@students.undip.ac.id>
2024-12-02 10:36:02 +0700

    add CRUD for note API

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,65 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { prisma } from "./utils/prisma";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", async () => {
+    const notes = await prisma.note.findMany();
+    return notes;
+  })
+  .post("/", async ({body, set}) => {
+    const { content } = body;
+    const newNote = await prisma.note.create({
+      data: {
+        content
+      }
+    })
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+    set.status = 201;
+    return newNote;
+  }, {
+    body: t.Object({
+      content: t.String()
+    })
+  }) //create note
+  .get("/:id", async ({ params }) => {
+    const {id} = params;
+    const note = await prisma.note.findFirst({
+      where: {
+        id
+      }
+    });
+
+    return id;
+  }) //get one note
+  .patch("/:id", async ({ params, body }) => {
+    const {id} = params;
+    const { content, isDone } = body;
+    const updatedNote = await prisma.note.update({
+      where: {
+        id
+      },
+      data: {
+        content,
+        isDone
+      }
+    })
+
+    return updatedNote;
+  }, {
+    body: t.Object({
+      content: t.String(),
+      isDone: t.Boolean()
+    })
+  }) //update note
+  .delete("/:id", async ({params}) => {
+    const {id} = params;
+    await prisma.note.delete({
+      where: {
+        id
+      }
+    })
+    return 'note deleted'
+  }) //delete note
+  .listen(3000);
+
+console.log("NoteBE ran at 3000")
